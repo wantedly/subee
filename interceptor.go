643f01b602297442051041a832e895da066c7173
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2019-03-08 00:13:46 +0900

    Tweak

--- interceptor.go
+++ interceptor.go
@@ -9,7 +9,7 @@ import (
 // BatchConsumerFunc type is an adapter to allow the use of ordinary functions as BatchConsumer.
 type BatchConsumerFunc func(context.Context, []Message) error
 
-// Consume call f(ctx, msgs)
+// BatchConsume call f(ctx, msgs)
 func (f BatchConsumerFunc) BatchConsume(ctx context.Context, msgs []Message) error {
 	return errors.WithStack(f(ctx, msgs))
 }
@@ -25,7 +25,7 @@ func (f ConsumerFunc) Consume(ctx context.Context, msg Message) error {
 	return errors.WithStack(f(ctx, msg))
 }
 
-// ConsumerInterceptor provides a hook to intercept the execution of a multiple message consumption.
+// ConsumerInterceptor provides a hook to intercept the execution of a message consumption.
 type ConsumerInterceptor func(Consumer) Consumer
 
 func chainConsumerInterceptors(consumer Consumer, interceptors ...ConsumerInterceptor) Consumer {
