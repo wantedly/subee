bd61da83aaae67ce7635d0557c8ac7c7f03ff7ca
izumin5210 <m@izum.in>
2019-09-25 10:16:14 +0900

    Stop watching signals, delegate it to `appctx.Global()`

--- engine.go
+++ engine.go
@@ -2,12 +2,8 @@ package subee
 
 import (
 	"context"
-	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/pkg/errors"
-	"golang.org/x/sync/errgroup"
 )
 
 // Engine is the framework instance.
@@ -45,44 +41,7 @@ func (e *Engine) Start(ctx context.Context) error {
 	e.Logger.Print("Start Pub/Sub worker")
 	defer e.Logger.Print("Finish Pub/Sub worker")
 
-	eg, ctx := errgroup.WithContext(ctx)
-	ctx, cancel := context.WithCancel(ctx)
 	ctx = setLogger(ctx, e.Logger)
 
-	sigDoneCh := make(chan struct{}, 1)
-
-	eg.Go(func() error {
-		return e.watchShutdownSignal(sigDoneCh, cancel)
-	})
-
-	eg.Go(func() error {
-		defer close(sigDoneCh)
-		return errors.WithStack(newProcess(e).Start(ctx))
-	})
-
-	err := eg.Wait()
-
-	return errors.WithStack(err)
-}
-
-func (e *Engine) watchShutdownSignal(sigstopCh <-chan struct{}, cancel context.CancelFunc) error {
-	sigCh := make(chan os.Signal, 1)
-
-	defer func() {
-		signal.Stop(sigCh)
-		close(sigCh)
-	}()
-
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-
-	for {
-		select {
-		case <-sigstopCh:
-			e.Logger.Print("Finish os signal monitoring")
-			return nil
-		case sig := <-sigCh:
-			e.Logger.Printf("Received signal: %v", sig)
-			cancel()
-		}
-	}
+	return errors.WithStack(newProcess(e).Start(ctx))
 }
